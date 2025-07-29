d9f0abcfdaa40cf23320dedf3e2305e75b269dc5
Abhinav Ranjan <ranjansme12@gmail.com>
2025-07-29 12:43:03 +0530

    binomial expression

--- Functions/functions.cpp
+++ Functions/functions.cpp
@@ -1,21 +1,47 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int sumOfDigit(int num){
-    int digitSum = 0;
-    while(num > 0){
-       int lastDigit = num % 10;
-        num = num/ 10;
-        digitSum += lastDigit;
+// int sumOfDigit(int num){
+//     int digitSum = 0;
+//     while(num > 0){
+//        int lastDigit = num % 10;
+//        cout << "Last Digit: " << lastDigit << endl; // Debugging output
+//         num = num / 10;
+//         cout << "Remaining Number: " << num << endl; // Debugging output
+//         digitSum += lastDigit;
+//     }
+//     return digitSum;
+// }
+
+int factorial(int num)
+{
+    int fact = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        fact *= i;
     }
-    return digitSum;
+    return fact;
+}
+int nCr(int n, int r)
+{
+    int a = factorial(n);
+    int b = factorial(r);
+    int a_b = factorial(n - r);
+    return a / (b * a_b);
 }
 
-int main() {
-    int number;
-    cout << "Enter a number: ";
-    cin >> number;
-    int result = sumOfDigit(number);
-    cout << "The sum of the digits is: " << result << endl;
+int main()
+{
+    // int number;
+    // cout << "Enter a number: ";
+    // cin >> number;
+    // int result = sumOfDigit(number);
+    // cout << "The sum of the digits is: " << result << endl;
+
+    int n, r;
+    cout << "Enter n and r for nCr: ";
+    cin >> n >> r;
+    int combination = nCr(n, r);
+    cout << "nCr(" << n << ", " << r << ") = " << combination << endl;
     return 0;
 }
